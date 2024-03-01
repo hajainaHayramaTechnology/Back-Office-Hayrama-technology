1abedb0664b0d43989d73c6399e81697f266928d
Andriantsitohaina Hajaina <74816899+mamyhajaina@users.noreply.github.com>
2024-03-01 17:24:48 +0300

    Encrypte and decrypte

--- src/main/java/com/Hayrama/services/TestService.java
+++ src/main/java/com/Hayrama/services/TestService.java
@@ -6,6 +6,10 @@ import java.util.List;
 import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 
 @Service
@@ -13,6 +17,61 @@ public class TestService {
 	
 	@Autowired
 	private TestRepository testRepo;
+	
+	private static final String secretKey = "secretKey";
+	
+	public static String encrypt(String inputData) {
+        try {
+            SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+
+            byte[] encryptedBytes = cipher.doFinal(inputData.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(encryptedBytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+	public static String decrypt(String inputData) {
+        try {
+            SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+
+            byte[] decodedBytes = Base64.getDecoder().decode(inputData);
+            byte[] decryptedBytes = cipher.doFinal(decodedBytes);
+            return new String(decryptedBytes, StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+	public static String permuterTroisDerniers(String str) {
+        if (str.length() % 3 == 0) {
+            // Diviser la chaîne en trois parties égales
+            String partie1 = str.substring(0, str.length() / 3);
+            String partie2 = str.substring(str.length() / 3, (2 * str.length()) / 3);
+            String partie3 = str.substring((2 * str.length()) / 3);
+            String troisDerniersPartie2 = partie2.substring(partie2.length() - 3);
+            String troisDerniersPartie3 = partie3.substring(partie3.length() - 3);
+
+            partie2 = partie2.substring(0, partie2.length() - 3) + troisDerniersPartie3;
+            partie3 = partie3.substring(0, partie3.length() - 3) + troisDerniersPartie2;
+
+            System.out.println("Avant permutation : [" + partie1 + "] [" + partie2 + "] [" + partie3 + "]");
+            System.out.println("Après permutation : [" + partie1 + "] [" + partie2 + "] [" + partie3 + "]");
+
+            return partie1 + partie2 + partie3;
+        } else {
+            System.out.println("La longueur de la chaîne n'est pas divisible par 3.");
+            return null;
+        }
+    }
+
 
 
 	public Test save(Test entity) {
