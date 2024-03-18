0cd20d2bfc332256359878cc7f04c7e33e02f47a
Andriantsitohaina Hajaina <74816899+mamyhajaina@users.noreply.github.com>
2024-03-18 22:18:46 +0300

    [POO]

--- src/main/java/com/Hayrama/services/EncryptDecryptService.java
+++ src/main/java/com/Hayrama/services/EncryptDecryptService.java
@@ -10,13 +10,32 @@ public class EncryptDecryptService {
 	
 	private static final String secretKey = "1234567890123456";
 	
+	public static String crypyJson(String inputData) {
+        try {
+            SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+            byte[] encryptedBytes = cipher.doFinal(inputData.getBytes());
+            String result = Base64.encodeBase64String(encryptedBytes);
+            System.out.println("result : " + result );
+            result = permuter(result);
+            return result;
+        } catch (Exception e) {
+            System.err.println("Encryption error: " + e.getMessage());
+            return null;
+        }
+    }
+	
 	public static String encryptStandard(String inputData) {
         try {
             SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "AES");
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
             byte[] encryptedBytes = cipher.doFinal(inputData.getBytes());
-            return Base64.encodeBase64String(encryptedBytes);
+            String result = Base64.encodeBase64String(encryptedBytes);
+            System.out.println("result : " + result );
+            result = permuter(result);
+            return result;
         } catch (Exception e) {
             System.err.println("Encryption error: " + e.getMessage());
             return null;
@@ -25,6 +44,7 @@ public class EncryptDecryptService {
 
 	public static String decryptStandard(String inputData) {
         try {
+        	inputData = resolvePermutation(inputData);
             byte[] decodedKey = secretKey.getBytes();
             SecretKeySpec secretKeySpec = new SecretKeySpec(decodedKey, "AES");
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
@@ -36,25 +56,88 @@ public class EncryptDecryptService {
             return null;
         }
     }
-
-	public static String permuterTroisDerniers(String str) {
-        if (str.length() % 3 == 0) {
-            String partie1 = str.substring(0, str.length() / 3);
-            String partie2 = str.substring(str.length() / 3, (2 * str.length()) / 3);
-            String partie3 = str.substring((2 * str.length()) / 3);
-            String troisDerniersPartie2 = partie2.substring(partie2.length() - 3);
-            String troisDerniersPartie3 = partie3.substring(partie3.length() - 3);
-
-            partie2 = partie2.substring(0, partie2.length() - 3) + troisDerniersPartie3;
-            partie3 = partie3.substring(0, partie3.length() - 3) + troisDerniersPartie2;
-
-            System.out.println("Avant permutation : [" + partie1 + "] [" + partie2 + "] [" + partie3 + "]");
-            System.out.println("Après permutation : [" + partie1 + "] [" + partie2 + "] [" + partie3 + "]");
-
-            return partie1 + partie2 + partie3;
-        } else {
-            System.out.println("La longueur de la chaîne n'est pas divisible par 3.");
+	
+	public static String encryptFix(String inputData) {
+        try {
+            inputData = "fh1" + inputData + "nt";
+            String result = encryptStandard(inputData);
+            return result;
+        } catch (Exception e) {
+            System.err.println("Encryption error: " + e.getMessage());
             return null;
         }
+    }
+	
+	public static String decryptFix(String inputData) {
+        try {
+            inputData = inputData.substring(0, 3);
+            inputData = inputData.substring(inputData.length() - 2);
+            String result = decryptStandard(inputData);
+            return result;
+        } catch (Exception e) {
+            System.err.println("Encryption error: " + e.getMessage());
+            return null;
+        }
+    }
+
+	public static String permuter(String str) {
+		if (str.length() < 9) {
+	        System.out.println("La longueur de la chaîne est inférieure à 9 et ne peut pas être divisée en trois parties de longueur égale.");
+	        return null;
+	    }
+    	String result = "";
+    	System.out.println("String : " + str );
+        String partie1 = str.substring(0, str.length() / 3);
+        String partie2 = str.substring(str.length() / 3, (2 * str.length()) / 3);
+        String partie3 = str.substring((2 * str.length()) / 3);
+        String troisDerniersPartie1 = partie1.substring(partie1.length() - 3);
+        String troisDerniersPartie2 = partie2.substring(partie2.length() - 3);
+        String troisDerniersPartie3 = partie3.substring(partie3.length() - 3);
+        
+        String sansTroisDerniers1 = partie1.replace(troisDerniersPartie1, "");
+        String sansTroisDerniers2 = partie2.replace(troisDerniersPartie2, "");
+        String sansTroisDerniers3 = partie3.replace(troisDerniersPartie3, "");
+        System.out.println("Parties : [" + partie1 + "] [" + partie2 + "] [" + partie3 + "]");
+        System.out.println("Trois Derniers Parties : [" + troisDerniersPartie1 + "] [" + troisDerniersPartie2 + "] [" + troisDerniersPartie3 + "]");
+        System.out.println("Sans Trois Derniers Parties : [" + sansTroisDerniers1 + "] [" + sansTroisDerniers2 + "] [" + sansTroisDerniers3 + "]");
+        partie1 = troisDerniersPartie2 + sansTroisDerniers1;
+        partie2 = troisDerniersPartie3 + sansTroisDerniers2;
+        partie3 = troisDerniersPartie1 + sansTroisDerniers3;
+        
+        result = partie1 + partie2 + partie3;
+        System.out.println("Parties Apres : [" + partie1 + "] [" + partie2 + "] [" + partie3 + "]");
+
+        return result;
+    }
+	
+	public static String resolvePermutation(String str) {
+		if (str.length() < 9) {
+	        System.out.println("La longueur de la chaîne est inférieure à 9 et ne peut pas être divisée en trois parties de longueur égale.");
+	        return null;
+	    }
+    	String result = "";
+    	System.out.println("resolvePermutation : " + str );
+        String partie1 = str.substring(0, str.length() / 3);
+        String partie2 = str.substring(str.length() / 3, (2 * str.length()) / 3);
+        String partie3 = str.substring((2 * str.length()) / 3);
+        
+        String troisPremierPartie1 = partie1.substring(0,3);
+        String troisPremierPartie2 = partie2.substring(0,3);
+        String troisPremierPartie3 = partie3.substring(0,3);
+        
+        String sansTroisPremier1 = partie1.replace(troisPremierPartie1, "");
+        String sansTroisPremier2 = partie2.replace(troisPremierPartie2, "");
+        String sansTroisPremier3 = partie3.replace(troisPremierPartie3, "");
+        System.out.println("Parties : [" + partie1 + "] [" + partie2 + "] [" + partie3 + "]");
+        System.out.println("Trois Derniers Parties : [" + troisPremierPartie1 + "] [" + troisPremierPartie2 + "] [" + troisPremierPartie3 + "]");
+        System.out.println("Sans Trois Derniers Parties : [" + sansTroisPremier1 + "] [" + sansTroisPremier2 + "] [" + sansTroisPremier3 + "]");
+        partie1 = sansTroisPremier1 + troisPremierPartie3;
+        partie2 = sansTroisPremier2 + troisPremierPartie1;
+        partie3 = sansTroisPremier3 + troisPremierPartie2;
+        
+        result = partie1 + partie2 + partie3;
+        System.out.println("Parties Apres : [" + partie1 + "] [" + partie2 + "] [" + partie3 + "]");
+
+        return result;
     }
 }
