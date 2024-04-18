a521a5101b7fe23e71a573352d211647deb04ca4
hajainaHayramaTechnology <160156114+hajainaHayramaTechnology@users.noreply.github.com>
2024-04-18 18:40:02 +0200

    [Web Socket]

--- src/main/java/com/Hayrama/configue/WebSocketConfig.java
+++ src/main/java/com/Hayrama/configue/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.Hayrama.configue;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -11,11 +12,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/websocket").withSockJS();
+		registry.addEndpoint("/websocket")
+				.setAllowedOriginPatterns("*");
 	}
 	
 	@Override
-	public void configureMessageBroker(org.springframework.messaging.simp.config.MessageBrokerRegistry registry) {
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
 		registry.setApplicationDestinationPrefixes("app");
 		registry.enableSimpleBroker("topic");
 	}
