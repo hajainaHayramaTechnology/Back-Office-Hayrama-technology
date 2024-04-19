25f3241b25ba65a47bdf9542415fe372daae01dd
hajainaHayramaTechnology <160156114+hajainaHayramaTechnology@users.noreply.github.com>
2024-04-19 13:35:12 +0200

    [Fix Bug]

--- src/main/java/com/Hayrama/configue/WebSocketConfig.java
+++ src/main/java/com/Hayrama/configue/WebSocketConfig.java
@@ -3,8 +3,8 @@ package com.Hayrama.configue;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
 @Configuration
 @EnableWebSocketMessageBroker
--- src/main/java/com/Hayrama/controllers/ChatController.java
+++ src/main/java/com/Hayrama/controllers/ChatController.java
@@ -1,10 +1,12 @@
 package com.Hayrama.controllers;
 
 import java.sql.Date;
+import java.util.Objects;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 import com.Hayrama.models.ChatMessage;
@@ -12,10 +14,16 @@ import com.Hayrama.models.ChatMessage;
 @Controller
 public class ChatController {
 
-	@MessageMapping("/chat")
-	@SendTo("/topic/message")
+	@MessageMapping("/chat.register")
+	@SendTo("/topic/public")
+	public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+		Objects.requireNonNull(headerAccessor.getSessionAttributes()).put("userName", chatMessage.getNickName());
+		return chatMessage;
+	}
+	
+	@MessageMapping("/chat.send")
+	@SendTo("/topic/public")
 	public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
-//		chatMessage.setTimestamp(new Date());
 		return chatMessage;
 	}
 }
